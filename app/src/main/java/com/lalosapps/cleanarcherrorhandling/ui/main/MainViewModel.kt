d8007719a1319524215838d23744c9ce5b9f6a91
cbarrios <lalobarrios16@yahoo.com>
2022-06-22 14:28:42 -0400

    master: submit email use case into view model

--- app/src/main/java/com/lalosapps/cleanarcherrorhandling/ui/main/MainViewModel.kt
+++ app/src/main/java/com/lalosapps/cleanarcherrorhandling/ui/main/MainViewModel.kt
@@ -4,8 +4,14 @@ import androidx.compose.runtime.getValue
 import androidx.compose.runtime.setValue
 import androidx.compose.runtime.mutableStateOf
 import androidx.lifecycle.ViewModel
+import androidx.lifecycle.viewModelScope
+import com.lalosapps.cleanarcherrorhandling.core.util.Resource
+import com.lalosapps.cleanarcherrorhandling.domain.SubmitEmailUseCase
+import kotlinx.coroutines.launch
 
-class MainViewModel : ViewModel() {
+class MainViewModel(
+    private val submitEmailUseCase: SubmitEmailUseCase = SubmitEmailUseCase()
+) : ViewModel() {
 
     var email by mutableStateOf("")
         private set
@@ -15,5 +21,12 @@ class MainViewModel : ViewModel() {
 
     fun onEmailChanged(email: String) {
         this.email = email
+    }
+
+    fun submitEmail() {
+        viewModelScope.launch {
+            val result = submitEmailUseCase(email)
+            if (result is Resource.Error) error = result.message
+        }
     }
 }
